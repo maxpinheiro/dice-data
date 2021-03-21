ece8e1be73ffa2f6c7a2b8a56a80e83ccd36e15e
Max Pinheiro <maxpinheiro181@gmail.com>
2021-03-20 20:44:46 -0400

    style

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function rollHistogram(data) {
         .append('g').attr('transform', `translate(${margin}, ${margin})`);
 
     const x = d3.scaleLinear().domain([1, 6]).range([0, width]);
-    svg1.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
+    svg1.append('g').attr('transform', `translate(0, ${height})`).style('stroke', 'rgb(150, 150, 150)').call(d3.axisBottom(x));
 
     const rolls = data.map(d => parseInt(d['roll 1'])).concat(data.map(d => parseInt(d['roll 2'])));
 
@@ -36,7 +36,7 @@ function rollHistogram(data) {
     const bins = histogram(rolls);
 
     const y = d3.scaleLinear().range([height, 0]).domain([0, d3.max(bins, d => d.length)]);
-    svg1.append('g').call(d3.axisLeft(y));
+    svg1.append('g').style('stroke', 'rgb(150, 150, 150)').call(d3.axisLeft(y));
 
     svg1.selectAll('rect').data(bins).enter().append('rect').attr('x', 1)
         .attr('transform', d => `translate(${x(d.x0)}, ${y(d.length)})`)
@@ -51,18 +51,18 @@ function sumHistogram(data) {
         .append('g').attr('transform', `translate(${margin}, ${margin})`);
 
     const x = d3.scaleLinear().domain([2, 12]).range([0, width]);
-    svg2.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
+    svg2.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x)).style('stroke', 'rgb(150, 150, 150)');
 
     const histogram = d3.histogram().value(d => d['sum']).domain(x.domain()).thresholds(x.ticks(9));
 
     const bins = histogram(data);
 
     const y = d3.scaleLinear().range([height, 0]).domain([0, d3.max(bins, d => d.length)]);
-    svg2.append('g').call(d3.axisLeft(y));
+    svg2.append('g').call(d3.axisLeft(y)).style('stroke', 'rgb(150, 150, 150)');
 
     svg2.selectAll('rect').data(bins).enter().append('rect').attr('x', 1)
         .attr('transform', d => `translate(${x(d.x0)}, ${y(d.length)})`)
         .attr('width', d => (x(d.x1) - x(d.x0) - 1))
         .attr('height', d => (height - y(d.length)))
-        .style('fill', '#9569b3');
+        .style('fill', '#258EA6');
 }
